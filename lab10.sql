e89c8086fbf3131d3ed4aeddca9b4595f8dc26ce
k6789j <Kelly.Jones1@marist.edu>
2016-04-27 20:23:20 -0400

    Lab 10
    Kelly Jones, Lab 10, 4/27/2016
    I have not yet attempted recursion but I will now.  Just wanted to put this up first

---
+++ lab10.sql
@@ -0,0 +1,54 @@
+﻿--1. Function PreReqsFor(courseNum) - returns the immediate prerequisites 
+--for the passed-in course number.
+
+create or replace function PreReqsFor(INT, REFCURSOR) RETURNS REFCURSOR AS
+$$
+declare
+   course      int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      SELECT num, name
+      FROM courses
+      WHERE num in (SELECT prereqnum
+                          FROM prerequisites
+                          WHERE courseNum = course);
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+
+
+select PreReqsFor(308, 'results');
+Fetch all from results;
+
+--2. Function IsPreReqFor(courseNum) - Returns the courses for which the 
+--passed-in course number is an immediate prerequisite.
+
+create or replace function IsPreReqFor(INT, REFCURSOR) RETURNS REFCURSOR AS
+$$
+declare
+   course      int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      SELECT num, name
+      FROM courses
+      WHERE num in (SELECT courseNum
+                          FROM prerequisites
+                          WHERE preReqNum = course);
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+
+
+select IsPreReqFor(120, 'results');
+Fetch all from results;
+
+
+
+
+
